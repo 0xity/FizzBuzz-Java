ddda922f472043cbd1ea2e1c40528ceed3092ecd
Oxity <oxity.productions@gmail.com>
2023-12-16 12:17:23 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+        StringBuilder output = new StringBuilder();
+        HashMap<Integer, String> values = new HashMap<>();
+        values.put(3, "Fizz");
+        values.put(5, "Buzz");
         for (int i = 1; i <= 100; i++) {
-            if (i % 15 == 0) {
-                System.out.println(i + " FizzBuzz!");
-            } else if (i % 3 == 0) {
-                System.out.println(i + " Fizz!");
-            } else if (i % 5 == 0) {
-                System.out.println(i + " Buzz!");
-            } else System.out.println(i);
+            for (Map.Entry<Integer, String> entry : values.entrySet()) {
+                if (i % entry.getKey() == 0) {
+                    output.append(entry.getValue());
+                }
+            }
+            System.out.println(i + " " + output);
+            output.delete(0, output.length());
         }
     }
 }
